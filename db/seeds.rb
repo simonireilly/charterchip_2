650e0d06949ad0781f72b2e3c6dcbdda2b7704c4
simon <simonireilly@gmail.com>
2017-12-09 17:28:25 +0000

    Adding everything so I can clone on the train

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 Evidence.destroy_all
 
+competencies = [
+    'Ai',
+    'Aii',
+    'Aiii',
+    'Aiv',
+    'Av',
+    'Bi',
+    'Bii',
+    'Biii',
+    'Ci ',
+    'Cii ',
+    'Ciii ',
+    'Di ',
+    'Dii ',
+    'Ei ',
+    'Eii'
+]
+
 30.times do |time|
-Evidence.create(competency: "Aii",
-                level: 2,
+Evidence.create(competency: competencies.sample,
+                level: rand(1..4),
                 title: "Example competency evidence",
                 summary: "Working on this cool dance! I haven't got it finished yet, but when I do I think it will be great.",
                 report: "<p>This is the #{time} piece of evidence, we want to fix a few things:</p><ol><li>Updated at and recently edited needs to be nice</li><li>We need to implement filtering and we need to have some colour coding</li><li>We needto look into getting spell check [might not need this in certain browsers]</li></ol>",
