c6a444361fa95f53f775f04a1a9f4e3702b73d62
simon <simonireilly@gmail.com>
2017-12-10 15:42:44 +0000

    Removed attching file model etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :evidences
-  mount Ckeditor::Engine => '/ckeditor'
   root to: 'evidences#index'
   #devise_for :users, path: '', path_names: { sign_in: '/', sign_out: '/', sign_up: '/'}
 
